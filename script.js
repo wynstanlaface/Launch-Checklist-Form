c21dd5768a5dd01761e465eb5a93ea0fcacb41b4
Chris Bay <chris@chrisbay.net>
2022-07-26 07:20:45 -0500

    Further starter code cleanup

--- script.js
+++ script.js
@@ -2,18 +2,12 @@
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
-<ol>
+<ul>
    <li>Name: ${}</li>
    <li>Diameter: ${}</li>
    <li>Star: ${}</li>
    <li>Distance from Earth: ${}</li>
    <li>Number of Moons: ${}</li>
-</ol>
+</ul>
 <img src="${}">
 */
-
-window.addEventListener("load", function(){
-
-  
-
-});
